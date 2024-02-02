5a79e8c3dd329fe506b8e1ef97e557a6c4f8e5f6
32GB <np05cp4a220025@iic.edu.np>
2024-02-02 16:44:13 +0545

    Not Much

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,4 @@ const {
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/products", viewProducts);
-router.get("/test",(req,res)=>{
-  res.send("Nice")
-});
 module.exports = { indexRouter: router };
