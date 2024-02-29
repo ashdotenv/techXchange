5722a4a2becafef5fd50ce74f4bdd24866ec2ef6
32GB <np05cp4a220025@iic.edu.np>
2024-02-29 20:33:46 +0545

    Cookie Secured

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const port = PORT || 5000;
 const bodyParser = require("body-parser");
 const router = require("./routes");
 const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+app.use(cookieParser(COOKIE_SECRET));
 const { userRouter } = require("./routes/user");
 const { default: mongoose } = require("mongoose");
 const { adminRouter } = require("./routes/adminRoutes");
