093d175572532163cd0da379f5186be27dfcfb60
ashishdotenv <155558827+ashishdotenv@users.noreply.github.com>
2024-02-29 20:29:10 +0545

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const JWT_SECRET = process.env.JWT_SECRET;
-const COOKIE_SECRET = process.env.JWT_SECRET;
+const COOKIE_SECRET = process.env.COOKIE_SECRET;
 const PORT = process.env.PORT;
 const DB_URI = process.env.DB_URI;
 const CLOUDINARY_API_SECRET=process.env.CLOUDINARY_API_SECRET;
